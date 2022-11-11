7b8f735aa3d0850af94bf9d255826c3d18499e28
weilin <weilin.huang@renren-inc.com>
2022-11-11 22:00:05 +0800

    react learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import  ReactDOM   from 'react-dom'
+//import  ReactDOM   from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 const name = 'LICSLAN'
 
 
+//JXS
 const title = (
     <h1>hello {name}, You are learning react!</h1>
 )
@@ -29,8 +31,34 @@ class Hello extends React.Component{
     }
 
 }
+console.log(Hello)
 
-//ReactDOM.render(title, document.getElementById('root'))
 
 
-ReactDOM.render(<Hello />, document.getElementById('root'))
+const songs = [
+    {id:1, name:'vue1'},
+    {id:2, name:'vue2'},
+    {id:3, name:'vue3'}
+]
+
+
+const list = (
+    <ul>
+        {
+            songs.map(item=><li key={item.id}>{item.name}</li>)
+        }
+    </ul>
+)
+   
+
+
+//v17.0
+
+//ReactDOM.render(list, document.getElementById('root'))
+
+//React团队在3月29日新推出了React v18.0版本，现在npm 默认的就是18版本，而React 18 不再支持 ReactDOM.render，
+createRoot(document.getElementById('root')).render(list)
+
+
+
+//ReactDOM.render(<Hello />, document.getElementById('root'))
